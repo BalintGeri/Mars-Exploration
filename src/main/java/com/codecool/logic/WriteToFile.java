c86888b9903d435c690f27ca9e05a112a4de6ef4
B.Geri <baligergely@gmail.com>
2023-06-05 14:23:59 +0200

    add FileWriter and user input handler

---
+++ src/main/java/com/codecool/logic/WriteToFile.java
@@ -0,0 +1,32 @@
+package com.codecool.logic;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+
+public class WriteToFile {
+    MapConfig config;
+    public WriteToFile(MapConfig config) {
+        this.config = config;
+    }
+
+    public void printMapToConsole(LinkedHashMap<Coordinate, ElementType> map) {
+        for (int x = 0; x < this.config.getSize(); x++) {
+            for (int y = 0; y < this.config.getSize(); y++) {
+                System.out.print(map.get(new Coordinate(x,y)).getValue());
+            }
+            System.out.println();
+        }
+    }
+
+    public void saveMapToFile(LinkedHashMap<Coordinate, ElementType> map, String fileName) throws IOException {
+        FileWriter writer = new FileWriter(Data.LOG_PATH + fileName + ".txt");
+        for (int i = 0; i < this.config.getSize(); i++) {
+            for (int j = 0; j < this.config.getSize(); j++) {
+                writer.write(map.get(new Coordinate(i,j)).getValue());
+            }
+            writer.write("\n");
+        }
+        writer.close();
+    }
+}
---
+++ src/main/java/com/codecool/ui/GetUserInput.java
@@ -0,0 +1,35 @@
+package com.codecool.ui;
+
+import java.util.Scanner;
+
+public class GetUserInput {
+    private int size;
+    private int noOfMTN;
+    private int noOfPit;
+    private String fileName;
+
+    public int getNoOfMTN() {
+        return noOfMTN;
+    }
+
+    public int getNoOfPit() {
+        return noOfPit;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void promptInput() {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Please enter map configuration \n File name for output \n Size of map \n Number of mountain groups \n Number of pit groups \n thanks");
+        fileName = myObj.nextLine();
+        size = myObj.nextInt();
+        noOfMTN = myObj.nextInt();
+        noOfPit = myObj.nextInt();
+    }
+}
