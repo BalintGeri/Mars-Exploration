808f5565e0d3e2ba01c3ddca918ef8e20a281f42
B.Geri <baligergely@gmail.com>
2023-06-05 14:05:22 +0200

    add MapConfig

---
+++ src/main/java/com/codecool/data/MapConfig.java
@@ -0,0 +1,31 @@
+package com.codecool.data;
+
+public class MapConfig {
+    private final int size;
+    private final int noOfMtns;
+    private final int noOfPits;
+    private final String fileName;
+
+    public MapConfig(GetUserInput input) {
+        this.size = input.getSize();
+        this.noOfMtns = input.getNoOfMTN();
+        this.noOfPits = input.getNoOfPit();
+        this.fileName = input.getFileName();
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getNoOfMtns() {
+        return noOfMtns;
+    }
+
+    public int getNoOfPits() {
+        return noOfPits;
+    }
+}
